9280fe444cfe52085446c09a1c2e9309e2e7f107
TaeYoonSS <lty0880@gmail.com>
2023-01-31 14:20:49 +0900

    Update code06_list.py

--- Day02/code06_list.py
+++ Day02/code06_list.py
@@ -41,3 +41,18 @@ print(tuple1)
 arr5.append(4)
 print(arr5)
 
+# 딕셔너리 
+spiderman = {'name' : 'Peter Parker', 
+            'age' : 18,
+            'weapon' : 'web shooter'}
+print(spiderman['name'])
+print(spiderman['age'])
+print(spiderman['weapon'])
+print(type(spiderman))
+
+# 집합
+set1 = set([1,2,3,4])
+print(set1)
+
+set1 = set("Hello World")
+print(set1)
