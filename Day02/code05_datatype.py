f0238e9e268c424d7f2ee642882f5d53556c7199
TaeYoonSS <lty0880@gmail.com>
2023-01-31 12:24:17 +0900

    update

--- Day02/code05_datatype.py
+++ Day02/code05_datatype.py
@@ -13,9 +13,6 @@ print(type(val))
 val = 3.14
 print(type(val))
 
-val = "Hello"
-print(type(val))
-
 val = 0b1010
 print(type(val))
 
@@ -27,3 +24,40 @@ print(val)
 
 val = 3_099.99
 print(val)
+
+# 문자열
+val = 'Life is short, You need Python'
+print(val)
+print(type(val))
+
+val = 'Hello\nWorld!' # 탈출문자
+print(val)
+val = 'Hello\tWorld!'
+print(val)
+val = 'Hello\t\bWorld!'
+print(val)
+
+val = '''Life is short, 
+You need Python'''
+print(val)
+
+val = 'Hello World!'
+val = "Hello World!"
+
+val = "Hi, I'm 'ty'"
+print(val)
+val = 'Hi, I\'m \'ty\''
+print(val)
+
+# 불린형 or 불
+참 = True
+거짓 = False
+print(type(거짓))
+print(1+1 == 2)
+print(거짓 == True)
+print(거짓 == False)
+print(거짓 is False)
+
+print(bool(1)) # 1 == True
+print(bool(0)) # 0 == False
+print(bool(2)) # 1 이외 값은 True 라고 하지마세용
---
+++ Day02/code06_list.py
@@ -0,0 +1,29 @@
+# 복합형 
+ 
+# 리스트 안씀
+a1 = 1
+a2 = 2
+a3 = 3
+a4 = 4
+a5 = 5
+print(a1 + a2 + a3 + a4 + a5)
+print(a1,a2,a3,a4,a5)
+
+# 리스트 , 리스트 == 배열 in py
+
+a = [1,2,3,4,5]
+sum = 0
+for i in a :
+    sum += i
+print(sum)
+print(a)
+
+arr1 = [1,2,3]
+arr2 = [1.1,2.2,3.3]
+arr3 = ['Hello',13,'World!',True]
+print(arr3)
+print(type(arr3))
+
+arr4 = [] # 빈 리스트
+arr5 = list()
+print(arr5)
