07909f510b8149a66a8f9a481724b2070cbd2c7d
TaeYoonSS <lty0880@gmail.com>
2023-01-31 11:00:07 +0900

    Create code04_variable.py

---
+++ Day02/code04_variable.py
@@ -0,0 +1,17 @@
+# 변수
+
+val = 'Hello'
+print(val)
+print(type(val))
+
+val=3.14159
+print(val)
+print(type(val))
+
+val=10/2
+print(id(val))
+print(type(val))
+
+# 변수명 단어들의 조합
+plant_major_upper_code = 'U13TEMP'
+print(id(plant_major_upper_code))
