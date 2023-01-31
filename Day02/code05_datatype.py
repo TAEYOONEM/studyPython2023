02a1a76f4ad41ce0f02b50b00934b88175a5c44c
TaeYoonSS <lty0880@gmail.com>
2023-01-31 11:29:34 +0900

    Update code05_datatype.py

---
+++ Day02/code05_datatype.py
@@ -0,0 +1,29 @@
+# 자료형
+# None 값이 없는 값
+None # 컴퓨터왈: 난몰라~~
+
+print(None)
+print(0==None)
+print(''==None)
+
+# 숫자형
+val = 3
+print(type(val))
+
+val = 3.14
+print(type(val))
+
+val = "Hello"
+print(type(val))
+
+val = 0b1010
+print(type(val))
+
+val = 12.123
+print(type(val))
+
+val = 4_520_000
+print(val)
+
+val = 3_099.99
+print(val)
