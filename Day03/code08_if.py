b43e7717d746a915b014991d7a67ce4ff28927cc
TaeYoonSS <lty0880@gmail.com>
2023-02-01 10:09:31 +0900

    일단올림

---
+++ Day03/code08_if.py
@@ -0,0 +1,14 @@
+name = '태윤'
+state = '아픔'
+
+if name == '태윤' :
+    print("qwe")
+    if state == '아픔' :
+        print('rty')
+    else :
+        print('iop')
+elif name == '찬욱' : 
+    print('asd')
+else :
+    print("zxc") 
+
