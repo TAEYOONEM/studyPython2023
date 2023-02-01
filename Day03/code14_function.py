ef81b678bbcce938586ee779a61edeb89a10db0c
TaeYoonSS <lty0880@gmail.com>
2023-02-01 14:36:37 +0900

    update

--- Day03/code14_function.py
+++ Day03/code14_function.py
@@ -1,5 +1,33 @@
-# 함수
-def add(x,y) :
-    return x + y
+# # 함수
 
-print(add(1,2))
+# global cnt
+# cnt = 0
+
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+n = int(input())
+for i in range(1,n+1) :
+    val = input()
+    cnt = 0
+    print(isPalindrome(val),cnt)
+
+
+# def add(x,y) :
+#     return x + y
+
+# def sub(x,y) :
+#     return x - y
+
+# def mul(x,y) :
+#     return x * y
+
+# def div(x,y) :
+#     return x/y
---
+++ Day03/code15_function2.py
@@ -0,0 +1,13 @@
+def add(x,y) :
+    print(x + y)
+
+def sub(x,y) :
+    print(x - y)
+    
+def mul(x,y) :
+    print(x * y)
+    
+def div(x,y) :
+    print(x // y)
+    
+add(1,2)
