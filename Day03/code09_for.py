cd9d43ede93c8739d9b763e5449f3c5a812d6019
TaeYoonSS <lty0880@gmail.com>
2023-02-01 10:32:09 +0900

    Update code09_for.py

--- Day03/code09_for.py
+++ Day03/code09_for.py
@@ -1,4 +1,22 @@
 arr = [1, 2, 3, 4, 5]
+sum = 0 
 
 for i in arr :
-    print(f'{i:2.2f}')
+    # print(f'{i:.2f}')
+    sum += i
+
+# print('sum ='+str(sum))
+print(f'sum ={sum}')
+
+
+# 리스트를 편하게 만드는 방법
+vals = [i for i in range(1,11)]
+print(vals)
+
+num = 0
+for i in vals : 
+    num += 1 
+    if num % 2 == 0 :
+        continue
+    else : 
+        print(num,'번 수는',i)
