72b5e936064cedd6271dd9c7bca2e4b24f5ffff5
TaeYoonSS <lty0880@gmail.com>
2023-02-06 12:40:07 +0900

    Update address_app.py

--- project/address_app.py
+++ project/address_app.py
@@ -1,6 +1,7 @@
 # 주소록 프로그램
 # 2023-02-06
 # TaeYoon
+import os # 운영체제용 모듈
 
 # 2. 클래스 생성
 class Contact:
@@ -19,13 +20,53 @@ class Contact:
                    f'주 소 : {self.__addr}\n') 
         return str_res
 
+# 5. 사용자입력
+def set_contact():
+    name, phone_num, email, addr = input('정보입력(이름/전번/이메일/주소:)').split('/')
+    #print(name,phone_num,email,addr)
+    
+    # 7 Contact 객체 만들기
+    # contact = Contact(addr=addr,email=email,name=name,phone_num=phone_num)
+    contact = Contact(name,phone_num,email,addr)
+    return contact
+
+# 추가. 화면클리어 
+def clear_console():
+    command = 'clear' # Linux, Unix 화면 클리어 명령어
+    if os.name in ('nt','dos') : # Window 운영체제라면
+        command = 'cls'
+    os.system(command)
+
+# 6. 메뉴표시
+def get_menu(): 
+    str_menu = ('주소록앱 ver0.5\n'
+                '1. 연락처 추가\n'
+                '2. 연락처 출력\n'
+                '3. 연락처 삭제\n'
+                '4. 앱 종료')
+    print(str_menu)
+    menu = int(input('메뉴입력:'))
+    return menu
+
 # 3. 전체 실행
 def run() :
-    temp = Contact('엄준식','010-0000-0000','aaa@aaa.com',
-                    '동탄시...')
-    print(temp)
+    contacts = list() # 주소를 담기 위한 빈 리스트 생성
+    # temp = Contact('엄준식','010-0000-0000','aaa@aaa.com',
+    #                 '동탄시...')
+    # set_contact()
+    clear_console()
+    while True :
+        sel_menu = get_menu()
+        if sel_menu == 1 : # 8. 연락처 추가
+            clear_console()
+            contact = set_contact()
+            contacts.append(contact) 
+        elif sel_menu == 4 :
+            break
+        else : 
+            clear_console()
+                 
 
 # 1. 메인 실행영역 
 if __name__ == '__main__' :
-    print('주소록 앱 시작합니다.')
     run()
