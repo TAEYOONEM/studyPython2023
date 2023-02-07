3f1aac4d6ea28aed03f7401d9134fa0bfba04332
TaeYoonSS <lty0880@gmail.com>
2023-02-08 08:47:28 +0900

    update

--- test1.py
+++ test1.py
@@ -1,3 +1,12 @@
-size_rank = { "name" : ,
-            
-            }
+class SizeRank() :
+    w,h = 0  
+    rank = 
+
+sizerank = SizeRank()
+n = int(input())
+for i in range(n) :
+    sizerank.w[i],sizerank.h[i] = int(input().split(' '))
+print(sizerank.w)
+
+arr = list(map(int,input().split('\n')))
+print(arr)
