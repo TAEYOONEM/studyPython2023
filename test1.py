827c1047288c16ec9c3b7bd94c62cd9a31c09cda
TaeYoonSS <lty0880@gmail.com>
2023-02-08 17:29:15 +0900

    update

--- test1.py
+++ test1.py
@@ -1,13 +1,16 @@
 m,n = map(int,input().split())
 arr = list()
 
-#0 : 미판별 1: 소수x 2:소수
+for i in range(0,n+1) :
+    arr.append(0)
 
 for i in range(2,n+1) :
-    arr[i] = 0
+    if arr[i] != 0 :
+        continue
+    for j in range(2*i,n+1,i) :
+        arr[j] = 1
 
-for i in range(2,n+1) :
-    if arr[i] == 0 :
-        arr[i] = 2
-    for j in range(2,)    
-    
+for i in range(m,n+1) :
+    if(arr[i] == 0) :
+        print(i)
+
